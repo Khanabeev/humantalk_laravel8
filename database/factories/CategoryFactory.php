a22b8c547a4b30e04508b84153878c848fddc697
Anton Khanabeev <khanabeev@gmail.com>
2020-11-17 20:14:44 +0300

    add CategoryFactory

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -22,7 +22,11 @@ class CategoryFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'name' => $this->faker->name,
+            'image' => 'https://picsum.photos/700/467',
+            'slug' => $this->faker->slug,
+            'description' => $this->faker->realText(100),
+            'quantity' => $this->faker->numberBetween(1, 1000),
         ];
     }
 }
