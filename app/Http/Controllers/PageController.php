8dcf738bdb7d204c96b8955484d9b594af1e815f
Anton Khanabeev <khanabeev@gmail.com>
2020-11-17 22:10:09 +0300

    add RepositoryServiceProvider

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,13 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Repositories\PostRepository;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
+
+    /**
+     * @var PostRepository
+     */
+    private $postRepository;
+
+    public function __construct(PostRepository $postRepository)
+    {
+        $this->postRepository = $postRepository;
+    }
+
     public function index()
     {
-        $post = new Post();
 
 
     }
